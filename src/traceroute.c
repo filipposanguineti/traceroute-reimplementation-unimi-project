660bba3ee3f8dd830400ef793207822cc4cc13be
filipposanguineti <filippo.sanguineti@studenti.unimi.it>
2025-08-06 11:23:47 +0200

    check_ipv4 in progress, dnsresolver in progress

--- src/traceroute.c
+++ src/traceroute.c
@@ -1,14 +1,21 @@
 
 #include <stdio.h>
-#include <unistd.h> //è necessaria per usare getruid()
+#include <unistd.h> //è necessaria per usare geteuid()
 #include <arpa/inet.h> //necessaria per usare le funzioni di rete
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h> //necessaria per le funzioni di socket
+#include <netinet/in.h> //necessaria per le strutture di rete
+#include <netdb.h> //necessaria per le funzioni di risoluzione DNS
+#include <sys/types.h> //necessaria per estendere i tipi di dato
+
+#include "utils.h" //includo il file header per le dichiarazioni delle funzioni
 
 
 
 
 //dichiarazioni funzioni
-int check_ipv4(char *ip);
-void store_ip(char *ip, struct in_addr *ip_bin);
+void test_stampa_ip(struct in_addr ip_bin, char *ip_string);
 
 
 
@@ -41,6 +48,9 @@ int main(int argc, char *argv[]) {
         store_ip(ip_string, &ip_bin); //salvo l'ip in formato binario, passando la variabile per argomento
 
         printf("Valid IPv4 address provided: %s\n", argv[1]);
+
+        //test di stampa
+        test_stampa_ip(ip_bin, ip_string);
     
     }else if(check_ipv4(argv[1]) == 0) {
 
@@ -60,30 +70,20 @@ int main(int argc, char *argv[]) {
 
 
 
+void test_stampa_ip(struct in_addr ip_bin, char *ip_string) {
 
-int check_ipv4(char *ip){
-
-    struct in_addr buffer; //mi serve per permettere a inetpton di operare sull'ip con un buffer che ha la taglia di un ipv4
-
-    int val = inet_pton(AF_INET, ip, &buffer); //converte l'ip in un formato binario e salva il status code in val
-
-    //se val è 1: ip valido
-    //se val è 0: ip non valido
-    //se val è -1: errore
-
-    return val;
-
-}
-
-void store_ip(char *ip, struct in_addr *ip_bin) {
-
-    inet_pton(AF_INET, ip, ip_bin); //uso di nuovo inet_pton, ma ora salvo la variabile
-
-    if(ip_bin == NULL) {
-        fprintf(stderr, "Error, the address doesn't exist.\n");
+    //test di stampa sia dal formato binario, che da argv[]
+    char ip_str[INET_ADDRSTRLEN]; //buffer per la conversione da binario a stringa
+    if(inet_ntop(AF_INET, &ip_bin, ip_str, sizeof(ip_str)) == NULL) { //il contrario di inet_pton
+        fprintf(stderr, "Error converting binary IP to string.\n");
         return;
     }
-    
-    return;
+    printf("Binary IP: %s\n", ip_str);
+    printf("In string format: %s\n", ip_string); //stampo l'ip in formato stringa
 
 }
+
+
+
+
+
