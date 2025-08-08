04ea2fb4224ec6cfc4d5afb4432ba6e8c8554b37
filipposanguineti <filippo.sanguineti@studenti.unimi.it>
2025-08-08 11:12:28 +0200

    extraction in progress

--- src/icmp.c
+++ src/icmp.c
@@ -1,5 +1,7 @@
 
 #define _POSIX_C_SOURCE 200112L // Definisce macro per POSIX, necessaria per alcune funzioni di rete
+#define GNU_SOURCE 1// Definisce macro per GNU, necessaria per alcune funzioni di rete
+#define _DEFAULT_SOURCE 1 // Definisce macro per le funzioni di rete, necessaria per compatibilità con C17
 #include <stdio.h>
 #include <unistd.h> //è necessaria per usare geteuid()
 #include <arpa/inet.h> //necessaria per usare le funzioni di rete
@@ -9,6 +11,9 @@
 #include <netinet/in.h> //necessaria per le strutture di rete
 #include <netdb.h> //necessaria per le funzioni di risoluzione DNS
 #include <sys/types.h> //necessaria per estendere i tipi di dato
+#include <netinet/ip_icmp.h>
+#include <netinet/udp.h>
+
 
 #include "utils.h" //includo il file header per le dichiarazioni delle funzioni
 
@@ -48,6 +53,26 @@ int receive_icmp(int sd, char *buffer){
     }
 
     //il buffer dovrebbe essere pieno e può essere gestito dalla funzione in carica
+}
+
+
+int extract_rec_data(char *data, struct in_addr *addr, int *error){
+
+    //devo recuperare l'ip, e l'error code dal pacchetto ricevuto, e la porta usata per distinguere il probe giusto
+
+    //l'header completo è molto grande
+    //primi 20byte è l'header IP esterno (quello da cui si recupera l'indirizzo)
+    //poi 8 byte dell'header ICMP (quello da cui estraggo il type e il code di errore)
+    //poi 20byte di header ip interno (quello del probe iniziale)
+    //inifne 8 byte dell'header udp del probe iniziale (da cui estraggo la porta)
+
+    //mi devo muovere in ordine, saltando da un header all'altro fino ad arrivare in fondo
+
+    //estrazione ip
+    //dentro la struct iphdr c'è il campo saddr che contiene l'indirizzo IP del mittente, il valore è big-endian 
+    struct iphdr *ip_header = (struct iphdr *) data; //devo castare a iphdr per poter leggere correttamente i campi
+    addr->s_addr = ip_header->saddr; //metto l'ip dentro s-addr perché è il campo che lo deve contenere (diverso da inet_pton a cui si passa semplicemente la struct in_addr)
+
 }
 
 
